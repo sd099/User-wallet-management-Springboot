0c31b694065ae033cbc0205de703c1a94ae61509
sd099 <sdas680.sd@gmail.com>
2022-03-01 02:10:23 +0530

    Integration test complete with all positive and negative test cases

--- src/test/java/org/paytm/milestone2/_Milestone2/controllers/TransactionControllerTest.java
+++ src/test/java/org/paytm/milestone2/_Milestone2/controllers/TransactionControllerTest.java
@@ -47,78 +47,162 @@ class TransactionControllerTest {
 
     }
 
-    @Test
+    @Nested
     @DisplayName("Create transaction test")
     @Sql(statements = "insert into user (user_id,user_name,first_name,last_name,email_id,mobile_number,address_1,password) values (1000,\"Sd099\",\"Son\",\"Doe\",\"sondoe@gmail.com\",\"9876543210\",\"Time square, NY, USA\",\"$2a$10$hph6xo16D8ED1ZOHcMo7wuvrCH5ebxiVyTtPFGsHaZmIgAHWa55C2\")\n", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(statements = "insert into user (user_id,user_name,first_name,last_name,email_id,mobile_number,address_1,password) values (2000,\"Jd099\",\"John\",\"Doe\",\"johndoe@gmail.com\",\"0123456789\",\"Time square, NY, USA\",\"$2a$10$3IT1NBW1r60UtCPOXGlwweqmHJDwy3bsr4cuO4XkNnUguTtYgA8va\")\n", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(statements = "insert into wallet (wallet_id,mobile_number,current_balance) values (1000,\"9876543210\",5000.0)", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(statements = "insert into wallet (wallet_id,mobile_number,current_balance) values (2000,\"0123456789\",0.0)", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    void transactionP2P() throws Exception {
+    @Sql(statements = "DELETE FROM user", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Sql(statements = "DELETE FROM wallet", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Sql(statements = "DELETE FROM transaction", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public class transactionP2P{
+        @Test
+        @DisplayName("Create transaction test success")
+        void transactionP2PSuccess() throws Exception {
 
-        String signInRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/userObjects/signInRequestObject1.json")));
-        String jwtForUser = generateTokenUsingLogin(signInRequestBody);
-        String transactionCreateRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/transactionObjects/transactionP2pRequestObject1.json")));
+            String signInRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/userObjects/signInRequestObject1.json")));
+            String jwtForUser = generateTokenUsingLogin(signInRequestBody);
+            String transactionCreateRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/transactionObjects/transactionP2pRequestObject1.json")));
 
-        MvcResult result= mockMvc.perform(MockMvcRequestBuilders.post("/transaction")
-                        .header(HttpHeaders.AUTHORIZATION, "Bearer "+jwtForUser)
-                        .content(transactionCreateRequestBody)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn();
+            MvcResult result= mockMvc.perform(MockMvcRequestBuilders.post("/transaction")
+                            .header(HttpHeaders.AUTHORIZATION, "Bearer "+jwtForUser)
+                            .content(transactionCreateRequestBody)
+                            .contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andReturn();
 
-        String resultAsString = result.getResponse().getContentAsString();
-        MessageResponse response = objectMapper.readValue(resultAsString,MessageResponse.class);
+            String resultAsString = result.getResponse().getContentAsString();
+            MessageResponse response = objectMapper.readValue(resultAsString,MessageResponse.class);
 
-        assertNotNull(result);
-        assertEquals("Transaction Successfull",response.getMessage());
+            assertNotNull(result);
+            assertEquals("Transaction Successfull",response.getMessage());
+        }
+
     }
 
-    @Test
+    @Nested
     @DisplayName("View transaction by transaction Id")
     @Sql(statements = "insert into user (user_id,user_name,first_name,last_name,email_id,mobile_number,address_1,password) values (3000,\"Ss099\",\"Steve\",\"Smith\",\"stevesmith@gmail.com\",\"5432109876\",\"Time square, NY, USA\",\"$2a$10$7uLjRL1XR99ovnrin52/puncFBPdMKv79FQ8CZVi8UfGqcxv6Yfge\")\n", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(statements = "insert into transaction (txn_id,amount,payee_mobile_number,payer_mobile_number,status,timestamp) values (3000,200.0,\"0123456789\",\"5432109876\",\"SUCCESS\",\"1998-01-02 00:00:00.000\")\n", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(statements = "DELETE FROM user", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @Sql(statements = "DELETE FROM transaction", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-    void viewTransactionById() throws Exception {
+    public class viewTransactionById{
+        @Test
+        @DisplayName("View transaction by transaction Id success")
+        void viewTransactionByIdSuccess() throws Exception {
 
-        String signInRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/userObjects/signInRequestObject3.json")));
-        String jwtForUser = generateTokenUsingLogin(signInRequestBody);
-        int txnId = 3000;
+            String signInRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/userObjects/signInRequestObject3.json")));
+            String jwtForUser = generateTokenUsingLogin(signInRequestBody);
+            int txnId = 3000;
 
-        MvcResult result= mockMvc.perform(MockMvcRequestBuilders.get("/transaction")
-                        .header(HttpHeaders.AUTHORIZATION, "Bearer "+jwtForUser)
-                        .param("txnId",String.valueOf(txnId))
-                        .contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn();
+            MvcResult result= mockMvc.perform(MockMvcRequestBuilders.get("/transaction")
+                            .header(HttpHeaders.AUTHORIZATION, "Bearer "+jwtForUser)
+                            .param("txnId",String.valueOf(txnId))
+                            .contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andReturn();
 
-        String resultAsString = result.getResponse().getContentAsString();
-        Transaction response = objectMapper.readValue(resultAsString,Transaction.class);
+            String resultAsString = result.getResponse().getContentAsString();
+            Transaction response = objectMapper.readValue(resultAsString,Transaction.class);
 
-        assertEquals(txnId,response.getTxnId());
-        assertNotNull(result);
+            assertEquals(txnId,response.getTxnId());
+            assertNotNull(result);
+        }
+
+        @Test
+        @DisplayName("View transaction by transaction Id not found failure")
+        void viewTransactionByIdNotFound() throws Exception {
+
+            String signInRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/userObjects/signInRequestObject3.json")));
+            String jwtForUser = generateTokenUsingLogin(signInRequestBody);
+            int txnId = 2000;
+
+            MvcResult result= mockMvc.perform(MockMvcRequestBuilders.get("/transaction")
+                            .header(HttpHeaders.AUTHORIZATION, "Bearer "+jwtForUser)
+                            .param("txnId",String.valueOf(txnId))
+                            .contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andReturn();
+
+            String resultAsString = result.getResponse().getContentAsString();
+            MessageResponse response = objectMapper.readValue(resultAsString,MessageResponse.class);
+
+            assertNotNull(result);
+            assertEquals("Error: Transaction not exist",response.getMessage());
+        }
     }
 
-    @Test
+    @Nested
     @DisplayName("View transaction by user id")
     @Sql(statements = "insert into user (user_id,user_name,first_name,last_name,email_id,mobile_number,address_1,password) values (3000,\"Ss099\",\"Steve\",\"Smith\",\"stevesmith@gmail.com\",\"5432109876\",\"Time square, NY, USA\",\"$2a$10$7uLjRL1XR99ovnrin52/puncFBPdMKv79FQ8CZVi8UfGqcxv6Yfge\")\n", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(statements = "insert into user (user_id,user_name,first_name,last_name,email_id,mobile_number,address_1,password) values (1000,\"Sd099\",\"Son\",\"Doe\",\"sondoe@gmail.com\",\"9876543210\",\"Time square, NY, USA\",\"$2a$10$hph6xo16D8ED1ZOHcMo7wuvrCH5ebxiVyTtPFGsHaZmIgAHWa55C2\")\n", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(statements = "insert into transaction (txn_id,amount,payee_mobile_number,payer_mobile_number,status,timestamp) values (3000,200.0,\"0123456789\",\"5432109876\",\"SUCCESS\",\"1998-01-02 00:00:00.000\")\n", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    void viewTransactionByUserId() throws Exception {
-        String signInRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/userObjects/signInRequestObject3.json")));
-        String jwtForUser = generateTokenUsingLogin(signInRequestBody);
-        int userId = 3000;
-        int pageNo = 0;
+    @Sql(statements = "DELETE FROM user", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Sql(statements = "DELETE FROM transaction", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public class viewTransactionByUserId{
+        @Test
+        @DisplayName("View transaction by user id success")
+        void viewTransactionByUserIdSuccess() throws Exception {
+            String signInRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/userObjects/signInRequestObject3.json")));
+            String jwtForUser = generateTokenUsingLogin(signInRequestBody);
+            int userId = 3000;
+            int pageNo = 0;
 
-        MvcResult result= mockMvc.perform(MockMvcRequestBuilders.get("/transaction/"+userId)
-                        .header(HttpHeaders.AUTHORIZATION, "Bearer "+jwtForUser)
-                        .param("pageNo",String.valueOf(pageNo))
-                        .contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn();
+            MvcResult result= mockMvc.perform(MockMvcRequestBuilders.get("/transaction/"+userId)
+                            .header(HttpHeaders.AUTHORIZATION, "Bearer "+jwtForUser)
+                            .param("pageNo",String.valueOf(pageNo))
+                            .contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andReturn();
 
 
-        assertEquals(200,result.getResponse().getStatus());
-        assertNotNull(result);
+            assertEquals(200,result.getResponse().getStatus());
+            assertNotNull(result);
+        }
 
+        @Test
+        @DisplayName("View transaction by user id user not exist failure")
+        void viewTransactionByUserIdUserNotExist() throws Exception {
+            String signInRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/userObjects/signInRequestObject3.json")));
+            String jwtForUser = generateTokenUsingLogin(signInRequestBody);
+            int userId = 4000;
+            int pageNo = 0;
+
+            MvcResult result= mockMvc.perform(MockMvcRequestBuilders.get("/transaction/"+userId)
+                            .header(HttpHeaders.AUTHORIZATION, "Bearer "+jwtForUser)
+                            .param("pageNo",String.valueOf(pageNo))
+                            .contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andReturn();
+
+            String resultAsString = result.getResponse().getContentAsString();
+            MessageResponse response = objectMapper.readValue(resultAsString,MessageResponse.class);
+
+            assertNotNull(result);
+            assertEquals("Error: No user found",response.getMessage());
+        }
+
+        @Test
+        @DisplayName("View transaction by user id user unauthorized failure")
+        void viewTransactionByUserIdUserUnauthorized() throws Exception {
+            String signInRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/userObjects/signInRequestObject1.json")));
+            String jwtForUser = generateTokenUsingLogin(signInRequestBody);
+            int userId = 3000;
+            int pageNo = 0;
+
+            MvcResult result= mockMvc.perform(MockMvcRequestBuilders.get("/transaction/"+userId)
+                            .header(HttpHeaders.AUTHORIZATION, "Bearer "+jwtForUser)
+                            .param("pageNo",String.valueOf(pageNo))
+                            .contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andReturn();
+
+            String resultAsString = result.getResponse().getContentAsString();
+            MessageResponse response = objectMapper.readValue(resultAsString,MessageResponse.class);
+
+            assertNotNull(result);
+            assertEquals("Error: Unauthorized User",response.getMessage());
+        }
     }
 }
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -11,8 +11,8 @@
 --insert into user (user_id,user_name,first_name,last_name,email_id,mobile_number,address_1,password) values (1,"Sd099","Son","Doe","sondoe@gmail.com","9876543210","Time square, NY, USA","$2a$10$hph6xo16D8ED1ZOHcMo7wuvrCH5ebxiVyTtPFGsHaZmIgAHWa55C2")
 --insert into user (user_id,user_name,first_name,last_name,email_id,mobile_number,address_1,password) values (2,"Jd099","John","Doe","johndoe@gmail.com","0123456789","Time square, NY, USA","$2a$10$3IT1NBW1r60UtCPOXGlwweqmHJDwy3bsr4cuO4XkNnUguTtYgA8va")
 --insert into user (user_id,user_name,first_name,last_name,email_id,mobile_number,address_1,password) values (300,"Ss099","Steve","Smith","stevesmith@gmail.com","5432109876","Time square, NY, USA","$2a$10$7uLjRL1XR99ovnrin52/puncFBPdMKv79FQ8CZVi8UfGqcxv6Yfge")
-
+--
 --insert into wallet (wallet_id,mobile_number,current_balance) values (100,"9876543210",0.0)
 --insert into wallet (wallet_id,mobile_number,current_balance) values (1000,"0123456789",0.0)
-
+--
 --insert into transaction (txn_id,amount,payee_mobile_number,payer_mobile_number,status,timestamp) values (200,200.0,"0123456789","5432109876","SUCCESS","1998-01-02 00:00:00.000")
