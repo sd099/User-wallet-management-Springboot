19826d3cf959b7bc9405ef6bed173e915547f52c
sd099 <sdas680.sd@gmail.com>
2022-03-03 22:50:04 +0530

    More test cases added in transactionControllerTest

--- src/test/java/org/paytm/milestone2/_Milestone2/controllers/TransactionControllerTest.java
+++ src/test/java/org/paytm/milestone2/_Milestone2/controllers/TransactionControllerTest.java
@@ -51,11 +51,18 @@ class TransactionControllerTest {
     @DisplayName("Create transaction test")
     @Sql(statements = "insert into user (user_id,user_name,first_name,last_name,email_id,mobile_number,address_1,password) values (1000,\"Sd099\",\"Son\",\"Doe\",\"sondoe@gmail.com\",\"9876543210\",\"Time square, NY, USA\",\"$2a$10$hph6xo16D8ED1ZOHcMo7wuvrCH5ebxiVyTtPFGsHaZmIgAHWa55C2\")\n", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(statements = "insert into user (user_id,user_name,first_name,last_name,email_id,mobile_number,address_1,password) values (2000,\"Jd099\",\"John\",\"Doe\",\"johndoe@gmail.com\",\"0123456789\",\"Time square, NY, USA\",\"$2a$10$3IT1NBW1r60UtCPOXGlwweqmHJDwy3bsr4cuO4XkNnUguTtYgA8va\")\n", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(statements = "insert into user (user_id,user_name,first_name,last_name,email_id,mobile_number,address_1,password) values (3000,\"Ss099\",\"Steve\",\"Smith\",\"stevesmith@gmail.com\",\"5432109876\",\"Time square, NY, USA\",\"$2a$10$7uLjRL1XR99ovnrin52/puncFBPdMKv79FQ8CZVi8UfGqcxv6Yfge\")\n", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(statements = "insert into wallet (wallet_id,mobile_number,current_balance) values (1000,\"9876543210\",5000.0)", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(statements = "insert into wallet (wallet_id,mobile_number,current_balance) values (2000,\"0123456789\",0.0)", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(statements = "DELETE FROM user", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @Sql(statements = "DELETE FROM wallet", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @Sql(statements = "DELETE FROM transaction", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+
+    //1 Son doe
+    //2 John doe
+    //3 Steve Smith
+    //1 son doe wallet money
+    //2 john doe wallet
     public class transactionP2P{
         @Test
         @DisplayName("Create transaction test success")
@@ -77,6 +84,132 @@ class TransactionControllerTest {
 
             assertNotNull(result);
             assertEquals("Transaction Successfull",response.getMessage());
+        }
+        @Test
+        @DisplayName("Create transaction test payer not found failure")
+        void transactionP2PPayerNotFound() throws Exception {
+
+            String signInRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/userObjects/signInRequestObject1.json")));
+            String jwtForUser = generateTokenUsingLogin(signInRequestBody);
+            String transactionCreateRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/transactionObjects/transactionP2pRequestObject2.json")));
+
+            MvcResult result= mockMvc.perform(MockMvcRequestBuilders.post("/transaction")
+                            .header(HttpHeaders.AUTHORIZATION, "Bearer "+jwtForUser)
+                            .content(transactionCreateRequestBody)
+                            .contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andReturn();
+
+            String resultAsString = result.getResponse().getContentAsString();
+            MessageResponse response = objectMapper.readValue(resultAsString,MessageResponse.class);
+
+            assertNotNull(result);
+            assertEquals("Error: Payer User Not Found with this Mobile Number",response.getMessage());
+        }
+        @Test
+        @DisplayName("Create transaction test user unauthorized failure")
+        void transactionP2PUserUnauthorized() throws Exception {
+
+            String signInRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/userObjects/signInRequestObject2.json")));
+            String jwtForUser = generateTokenUsingLogin(signInRequestBody);
+            String transactionCreateRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/transactionObjects/transactionP2pRequestObject1.json")));
+
+            MvcResult result= mockMvc.perform(MockMvcRequestBuilders.post("/transaction")
+                            .header(HttpHeaders.AUTHORIZATION, "Bearer "+jwtForUser)
+                            .content(transactionCreateRequestBody)
+                            .contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andReturn();
+
+            String resultAsString = result.getResponse().getContentAsString();
+            MessageResponse response = objectMapper.readValue(resultAsString,MessageResponse.class);
+
+            assertNotNull(result);
+            assertEquals("Error: Unauthorized User",response.getMessage());
+        }
+        @Test
+        @DisplayName("Create transaction test payer wallet not found failure")
+        void transactionP2PPayerWalletNotFound() throws Exception {
+
+            String signInRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/userObjects/signInRequestObject3.json")));
+            String jwtForUser = generateTokenUsingLogin(signInRequestBody);
+            String transactionCreateRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/transactionObjects/transactionP2pRequestObject3.json")));
+
+            MvcResult result= mockMvc.perform(MockMvcRequestBuilders.post("/transaction")
+                            .header(HttpHeaders.AUTHORIZATION, "Bearer "+jwtForUser)
+                            .content(transactionCreateRequestBody)
+                            .contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andReturn();
+
+            String resultAsString = result.getResponse().getContentAsString();
+            MessageResponse response = objectMapper.readValue(resultAsString,MessageResponse.class);
+
+            assertNotNull(result);
+            assertEquals("Error: Payer Wallet Not Found",response.getMessage());
+        }
+        @Test
+        @DisplayName("Create transaction test insufficient balance failure")
+        void transactionP2PInsufficientBalance() throws Exception {
+
+            String signInRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/userObjects/signInRequestObject1.json")));
+            String jwtForUser = generateTokenUsingLogin(signInRequestBody);
+            String transactionCreateRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/transactionObjects/transactionP2pRequestObject4.json")));
+
+            MvcResult result= mockMvc.perform(MockMvcRequestBuilders.post("/transaction")
+                            .header(HttpHeaders.AUTHORIZATION, "Bearer "+jwtForUser)
+                            .content(transactionCreateRequestBody)
+                            .contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andReturn();
+
+            String resultAsString = result.getResponse().getContentAsString();
+            MessageResponse response = objectMapper.readValue(resultAsString,MessageResponse.class);
+
+            assertNotNull(result);
+            assertEquals("Error: Insufficient balance",response.getMessage());
+        }
+        @Test
+        @DisplayName("Create transaction test payee wallet not found failure")
+        void transactionP2PPayeeWalletNotFound() throws Exception {
+
+            String signInRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/userObjects/signInRequestObject1.json")));
+            String jwtForUser = generateTokenUsingLogin(signInRequestBody);
+            String transactionCreateRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/transactionObjects/transactionP2pRequestObject5.json")));
+
+            MvcResult result= mockMvc.perform(MockMvcRequestBuilders.post("/transaction")
+                            .header(HttpHeaders.AUTHORIZATION, "Bearer "+jwtForUser)
+                            .content(transactionCreateRequestBody)
+                            .contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andReturn();
+
+            String resultAsString = result.getResponse().getContentAsString();
+            MessageResponse response = objectMapper.readValue(resultAsString,MessageResponse.class);
+
+            assertNotNull(result);
+            assertEquals("Error: Payee Wallet Not Found",response.getMessage());
+        }
+        @Test
+        @DisplayName("Create transaction test payer and payee same failure")
+        void transactionP2PPayerAndPayeeSame() throws Exception {
+
+            String signInRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/userObjects/signInRequestObject1.json")));
+            String jwtForUser = generateTokenUsingLogin(signInRequestBody);
+            String transactionCreateRequestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/jsonObject/transactionObjects/transactionP2pRequestObject6.json")));
+
+            MvcResult result= mockMvc.perform(MockMvcRequestBuilders.post("/transaction")
+                            .header(HttpHeaders.AUTHORIZATION, "Bearer "+jwtForUser)
+                            .content(transactionCreateRequestBody)
+                            .contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andReturn();
+
+            String resultAsString = result.getResponse().getContentAsString();
+            MessageResponse response = objectMapper.readValue(resultAsString,MessageResponse.class);
+
+            assertNotNull(result);
+            assertEquals("Error: Payer wallet and Payee wallet same. Try different wallet",response.getMessage());
         }
 
     }
